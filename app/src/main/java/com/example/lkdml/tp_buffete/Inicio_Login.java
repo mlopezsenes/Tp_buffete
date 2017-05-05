573865abf174a82de7e3536c5ff3f369144e3d80
lkdml <lazaro2002@hotmail.com>
2017-05-05 02:14:00 -0300

    1.1 Login sin validaciones

--- app/src/main/java/com/example/lkdml/tp_buffete/Inicio_Login.java
+++ app/src/main/java/com/example/lkdml/tp_buffete/Inicio_Login.java
@@ -9,5 +9,6 @@ public class Inicio_Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio__login);
+
     }
 }
