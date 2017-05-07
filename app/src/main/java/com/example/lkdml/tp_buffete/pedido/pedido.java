4403c80eb01109a1f0a0b217e6100ed5508f52ba
lkdml <lazaro2002@hotmail.com>
2017-05-07 14:56:44 -0300

    1.4 Mi Pedido
    Ademas se agregan ajustes en Menu y strings

---
+++ app/src/main/java/com/example/lkdml/tp_buffete/pedido/pedido.java
@@ -0,0 +1,15 @@
+package com.example.lkdml.tp_buffete.pedido;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.lkdml.tp_buffete.R;
+
+public class pedido extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pedido);
+    }
+}
